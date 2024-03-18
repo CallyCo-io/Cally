4c218217e90e2d1a6581ac0c2bb14c4349071724
Leon Wright <techman83@gmail.com>
2024-03-18 21:37:50 +0800

    test: Generalise Empty Synth test
    There isn't a strong need to test a full json payload. This avoids issues when
    the cdktf version changes

--- tests/stacks/test_cally.py
+++ tests/stacks/test_cally.py
@@ -8,4 +8,6 @@ class CallyStackTests(CallyTfTestHarness):
     def test_empty_synth(self):
         stack = CallyStack(service=self.empty_service)
         result = self.synth_stack(stack)
-        self.assertDictEqual(result, self.load_json_file('cdk/empty_synth.json'))
+        self.assertDictEqual(
+            result.get('terraform'), self.load_json_file('cdk/empty_synth.json')
+        )
