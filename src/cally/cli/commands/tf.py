32e6325312ebe46cc0073e67dcadfd84d9553341
Leon Wright <techman83@gmail.com>
2024-03-28 16:50:31 +0800

    feat: Write Template
    command to output terraform template as a json file

--- src/cally/cli/commands/tf.py
+++ src/cally/cli/commands/tf.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+
 import click
 
 from ..config import CallyConfig, service_options
@@ -17,4 +20,20 @@ def print_template(config: CallyConfig):
         click.secho(action.print())
 
 
+@click.command(name='write')
+@click.option(
+    '--output',
+    type=click.Path(path_type=Path),
+    default=Path(Path.cwd(), 'cdk.tf.json'),
+    help='Output path for the terraform json',
+)
+@service_options
+@click.pass_obj
+def write_template(config: CallyConfig, output: Path):
+    with terraform.Action(service=config.as_dataclass('CallyStackService')) as action:
+        output.write_text(action.print())
+        click.secho(f'Template written to {output}')
+
+
 tf.add_command(print_template)
+tf.add_command(write_template)
