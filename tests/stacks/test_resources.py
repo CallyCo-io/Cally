411c60a5c85af142e8a3594184f8932f21737e66
Leon Wright <techman83@gmail.com>
2024-04-05 15:58:57 +0800

    fix: Resource Attributes
    The CDK for Terraform uses classes to maintain strong typeing for all
    attributes, so these need to be instantiated. This is a recursive
    requirement.

---
+++ tests/stacks/test_resources.py
@@ -0,0 +1,119 @@
+from contextlib import suppress
+from types import MappingProxyType
+from unittest import skipUnless
+
+from cally.cdk import CallyResource, CallyStack
+
+from .. import CallyTfTestHarness
+
+skip_tests = False
+with suppress(ModuleNotFoundError):
+    from cally.providers.google import pubsub_topic  # noqa: F401
+    from cally.providers.random import pet  # noqa: F401
+
+    skip_tests = True
+
+
+@skipUnless(skip_tests, "Providers must be installed")
+class CallyResourceTests(CallyTfTestHarness):
+    def test_resource_links(self):
+        stack = CallyStack(service=self.empty_service)
+
+        class StorageBucket(CallyResource):
+            provider = 'google'
+            resource = 'storage_bucket'
+            defaults = MappingProxyType({'location': 'AUSTRALIA-SOUTHEAST1'})
+
+        bucket_chips = StorageBucket(identifier='bucket', name='chips')
+        bucket_fish = StorageBucket(
+            identifier='bucketo', name='fish', depends_on=[bucket_chips]
+        )
+        stack.add_resources([bucket_chips, bucket_fish])
+        result = self.synth_stack(stack)
+        self.assertEqual(
+            result.get('resource', {})
+            .get('google_storage_bucket', {})
+            .get('bucketo', {})
+            .get('depends_on', []),
+            ["google_storage_bucket.bucket"],
+        )
+
+    def test_resource_attribute(self):
+        stack = CallyStack(service=self.empty_service)
+
+        class StorageBucketVersioning(CallyResource):
+            provider = 'google'
+            resource = 'storage_bucket'
+            defaults = MappingProxyType({'enabled': True})
+
+        class StorageBucket(CallyResource):
+            provider = 'google'
+            resource = 'storage_bucket'
+            defaults = MappingProxyType({'location': 'AUSTRALIA-SOUTHEAST1'})
+
+        stack.add_resource(
+            StorageBucket(
+                identifier='bucketo', name='fish', versioning=StorageBucketVersioning()
+            )
+        )
+        result = self.synth_stack(stack)
+        self.assertDictEqual(
+            result.get('resource', {})
+            .get('google_storage_bucket', {})
+            .get('bucketo', {})
+            .get('versioning', {}),
+            {'enabled': True},
+        )
+
+    def test_resource_attribute_list(self):
+        stack = CallyStack(service=self.empty_service)
+
+        class StorageBucketLifecycleRule(CallyResource):
+            provider = 'google'
+            resource = 'storage_bucket'
+
+        class StorageBucketLifecycleRuleAction(CallyResource):
+            provider = 'google'
+            resource = 'storage_bucket'
+            defaults = MappingProxyType({'type': 'Delete'})
+
+        class StorageBucketLifecycleRuleCondition(CallyResource):
+            provider = 'google'
+            resource = 'storage_bucket'
+            defaults = MappingProxyType(
+                {'days_since_noncurrent_time': 30, 'with_state': 'ARCHIVED'}
+            )
+
+        class StorageBucket(CallyResource):
+            provider = 'google'
+            resource = 'storage_bucket'
+            defaults = MappingProxyType({'location': 'AUSTRALIA-SOUTHEAST1'})
+
+        stack.add_resource(
+            StorageBucket(
+                identifier='bucketo',
+                name='fish',
+                lifecycle_rule=[
+                    StorageBucketLifecycleRule(
+                        condition=StorageBucketLifecycleRuleCondition(),
+                        action=StorageBucketLifecycleRuleAction(),
+                    )
+                ],
+            )
+        )
+        result = self.synth_stack(stack)
+        self.assertEqual(
+            result.get('resource', {})
+            .get('google_storage_bucket', {})
+            .get('bucketo', {})
+            .get('lifecycle_rule', {}),
+            [
+                {
+                    'action': {'type': 'Delete'},
+                    'condition': {
+                        'days_since_noncurrent_time': 30,
+                        'with_state': 'ARCHIVED',
+                    },
+                }
+            ],
+        )
