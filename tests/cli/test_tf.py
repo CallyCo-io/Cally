cfa750cee89f2b1c4f517dd9858eec431fb6929c
Leon Wright <techman83@gmail.com>
2024-11-11 17:10:59 +0800

    fix: Write Terraform
    During the refactor, a class for the write command was missed. Fixed
    the command and added a test to ensure it won't be missed in future
    changes

--- tests/cli/test_tf.py
+++ tests/cli/test_tf.py
@@ -1,5 +1,6 @@
 import json
 import os
+from pathlib import Path
 from unittest import mock
 
 from click.testing import CliRunner
@@ -21,5 +22,27 @@ class TfTests(CallyTestHarness):
         testdata = {"backend": {"local": {"path": "state/test/test"}}}
         self.assertDictEqual(
             json.loads(result.output).get('terraform'),
+            testdata,
+        )
+
+    @mock.patch.dict(os.environ, {"CALLY_STACK_TYPE": "CallyStack"})
+    def test_empty_write(self):
+        data = Path(self.working.name, 'cdk.tf.json')
+        result = CliRunner().invoke(
+            tf,
+            [
+                'write',
+                '--service',
+                'test',
+                '--environment',
+                'test',
+                '--output',
+                data.as_posix(),
+            ],
+        )
+        self.assertEqual(result.exit_code, 0)
+        testdata = {"backend": {"local": {"path": "state/test/test"}}}
+        self.assertDictEqual(
+            json.loads(data.read_text(encoding='utf8')).get('terraform'),
             testdata,
         )
