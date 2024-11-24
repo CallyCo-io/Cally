efe9614984573f384e109bc62b302cbb2e0e1ded
Leon Wright <techman83@gmail.com>
2024-11-24 15:13:23 +0800

    feat: Run Terraform
    Give Cally the ability to run terraform directly, so that there is no need to mess
    around with outputs or stacks in the same directory conflicting with each other.

--- src/cally/cli/commands/tf.py
+++ src/cally/cli/commands/tf.py
@@ -1,4 +1,6 @@
+import sys
 from pathlib import Path
+from typing import Tuple
 
 import click
 
@@ -32,5 +34,32 @@ def write_template(config: CallyStackServiceConfig, output: Path):
         click.secho(f'Template written to {output}')
 
 
+@click.command(
+    name='run',
+    cls=CallyStackServiceCommand(),
+    context_settings={'ignore_unknown_options': True},
+)
+@click.option(
+    '--terraform-path',
+    envvar='CALLY_TERRAFORM_PATH',
+    type=click.Path(file_okay=True, dir_okay=False, executable=True),
+    default='/usr/bin/terraform',
+    help='Path to the terraform binary',
+)
+@click.argument('args', nargs=-1, type=click.UNPROCESSED)
+@click.pass_obj
+def run_terraform(
+    config: CallyStackServiceConfig, terraform_path: str, args: Tuple[str, ...]
+):
+    tf_cmd = terraform.Command(terraform_path=terraform_path, arguments=args)
+    with terraform.Action(service=config.config) as action:
+        action.synth_stack()
+        if not tf_cmd.success:
+            click.secho(message=tf_cmd.stderr, fg='red')
+            sys.exit(tf_cmd.returncode)
+    click.secho(tf_cmd.stdout)
+
+
 tf.add_command(print_template)
+tf.add_command(run_terraform)
 tf.add_command(write_template)
